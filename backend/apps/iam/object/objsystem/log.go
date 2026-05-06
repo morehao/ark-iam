09c403320f169cb081ea06ea5651c7c3e68d7b84
morehao <morehao@qq.com>
2026-05-07 00:56:27 +0800

    feat(iam): remove objsystem directory

--- backend/apps/iam/object/objsystem/log.go
+++
@@ -1,7 +0,0 @@
-package objsystem
-
-type LogBaseInfo struct {
-	TenantID uint   `json:"tenantID" form:"tenantID"` // 租户ID
-	Key      string `json:"key" form:"key"`           // 日志键
-	Payload  any    `json:"payload" form:"payload"`   // 日志内容
-}
--- backend/apps/iam/object/objsystem/system.go
+++
@@ -1,7 +0,0 @@
-package objsystem
-
-type SystemBaseInfo struct {
-	TenantID uint   `json:"tenantID" form:"tenantID"` // 租户ID
-	Key      string `json:"key" form:"key"`           // 配置键
-	Value    any    `json:"value" form:"value"`       // 配置值
-}
